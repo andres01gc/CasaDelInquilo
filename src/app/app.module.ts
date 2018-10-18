d1236d48bdd8f2a498e464d012498da99cda67ae
anfgc <moena1989@gmail.com>
2018-10-18 17:54:55 -0500

    firts commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {LoginComponent} from './main_routes/login/login.component';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './main_routes/home/home.component';
+
+
+const appRoutes: Routes = [{path: 'login', component: LoginComponent}, {path: 'home', component: HomeComponent}];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+}
